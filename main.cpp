fda6545217b6c7f033294059e04867bf37b36c1e
Ashley Chan <111904296+Ashleyc417@users.noreply.github.com>
2024-04-29 12:35:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,7 @@
 
 bool checkWinner(std::vector<std::vector<char>> &board) {
   // TODO: Implement the function checkWinner to check for if someone
-<<<<<<< HEAD
   // has won the game vertically or horizontally.
-=======
-  // has won the game vertically, horizontally, or diagonally. 
->>>>>>> 85bbfb45786d9bf97e40b62eabd993b900d3ccfa
   // If someone has won, return `true`, otherwise return false;
 
   return true;
@@ -16,28 +12,33 @@ bool checkWinner(std::vector<std::vector<char>> &board) {
 void printBoard(std::vector<std::vector<char>> &board) {
   // TODO: Implement the function printBoard to take in a gameboard
   // and print it out when called in main()
+  
 }
+
 
 int main() {
   std::cout << "Is there a winnner on the board?\n\n";
 
   // Test Cases
   std::vector<std::vector<char>> board1 = {
-      {'X', 'O', ' '}, {'X', 'O', ' '}, {'X', ' ', 'O'}};
-  std::cout << "Test Case 1: " << (checkWinner(board1) ? "true" : "false")
-            << std::endl;
+    {'X', 'O', ' '}, 
+    {'X', 'O', ' '}, 
+    {'X', ' ', 'O'}};
+  std::cout << "Test Case 1: " << (checkWinner(board1) ? "true" : "false") << "\n";
   printBoard(board1);
 
   std::vector<std::vector<char>> board2 = {
-      {'X', 'O', 'X'}, {'X', 'X', 'O'}, {'O', 'X', 'O'}};
-  std::cout << "Test Case 2: " << (checkWinner(board2) ? "true" : "false")
-            << std::endl;
+    {'X', 'O', 'X'}, 
+    {'X', 'X', 'O'}, 
+    {'O', 'X', 'O'}};
+  std::cout << "Test Case 2: " << (checkWinner(board2) ? "true" : "false") << "\n";
   printBoard(board2);
 
   std::vector<std::vector<char>> board3 = {
-      {'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', 'X', 'O'}};
-  std::cout << "Test Case 3: " << (checkWinner(board3) ? "true" : "false")
-            << std::endl;
+    {'O', 'X', 'X'}, 
+    {'X', 'O', ' '}, 
+    {'O', 'X', 'O'}};
+  std::cout << "Test Case 3: " << (checkWinner(board3) ? "true" : "false") << "\n";
   printBoard(board3);
 
   return 0;
